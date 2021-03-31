ef256457464d72dba06884bb2a23721764445a12
Wungiel <47087989+Syntetik008@users.noreply.github.com>
2021-03-31 19:59:16 +0200

    Le code fix

--- NitroGen.py
+++ NitroGen.py
@@ -9,3 +9,5 @@ while loopnum < int(many):  # loop
     result_str = ''.join((random.choice('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890') for i in range(16)))  # generate link
     print("https://discord.gift/" + result_str)  # show link
     loopnum += 1
+
+input("ok? (press any button)")
