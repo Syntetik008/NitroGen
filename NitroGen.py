13cab0cbee92aae082e68962730845ae9ef2dc84
Wungiel <47087989+Syntetik008@users.noreply.github.com>
2021-03-31 20:59:20 +0200

    Added output.txt
    all generated nitro links will be saved in output.txt

--- NitroGen.py
+++ NitroGen.py
@@ -1,13 +1,17 @@
 #imports
 import random
+import sys
 
 loopnum = 0
 
 many = input("How many to generate: ")  # asks for how many links to generate
 
+f= open("output.txt","w+")  # creating output.txt
+
 while loopnum < int(many):  # loop
     result_str = ''.join((random.choice('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890') for i in range(16)))  # generate link
     print("https://discord.gift/" + result_str)  # show link
+    f.write("https://discord.gift/" + result_str + "\n")  # adding links to output.txt
     loopnum += 1
 
 input("ok? (press any button)")
