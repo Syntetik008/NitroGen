e5c295612621af91dc3400ad811a715a86751d88
Wungiel <47087989+Syntetik008@users.noreply.github.com>
2021-03-31 19:53:05 +0200

    Le code

---
+++ NitroGen.py
@@ -0,0 +1,11 @@
+#imports
+import random
+
+loopnum = 0
+
+many = input("How many to generate: ")  # asks for how many links to generate
+
+while loopnum < int(many):  # loop
+    result_str = ''.join((random.choice('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890') for i in range(16)))  # generate link
+    print("https://discord.gift/" + result_str)  # show link
+    loopnum += 1
